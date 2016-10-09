f7377bfaf2c2e7118273519eb0add6d0ef050b5f
JK Pedapudi <jpedapudi@eastman.com>
2016-10-09 03:08:00 +0000

    Component Configuration

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,14 +1,17 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel",
 	"Walkthrough/model/models"
-], function(UIComponent, Device, models) {
+], function(UIComponent, Device, JSONModel, ResourceModel, models) {
 	"use strict";
 
 	return UIComponent.extend("Walkthrough.Component", {
 
 		metadata: {
-			manifest: "json"
+			manifest: "json",
+			rootView: "Walkthrough.view.App"
 		},
 
 		/**
@@ -22,6 +25,21 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set i18n model
+			var i18nModel = new ResourceModel({
+				bundleName: "Walkthrough.i18n.i18n"
+			});
+			this.setModel(i18nModel, "i18n");
 		}
 	});
 
